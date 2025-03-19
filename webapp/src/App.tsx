a8bc11f744a4043efb5e43016d76b0011e7297ab
Pavel <kpiligrin@mail.ru>
2025-03-19 21:45:36 +0400

    Add Layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
 import { getAllIdeasRoute, getViewIdeaRoute, viewIdeaRouteParams } from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
-
 export const App = () => {
   return (
     <TrpcProvider>
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          <Route element={<Layout />}>
+            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={getViewIdeaRoute(viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          </Route> {/* Закрывающий тег для Route с Layout */}
         </Routes>
       </BrowserRouter>
-  </TrpcProvider>
+    </TrpcProvider>
   )
 }
